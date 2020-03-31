abd381e8f68df9c5c2bfb6ad6318d04b9e1bbd58
Nikolaos IOANNOU <nikolaos.ioannou.e@thalesdigital.io>
2020-03-31 09:28:58 +0200

    add postgres database

--- config/settings.py
+++ config/settings.py
@@ -75,10 +75,20 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'myproject',
+        'USER': 'postgres',
+        'PASSWORD': 'adminpsql',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
