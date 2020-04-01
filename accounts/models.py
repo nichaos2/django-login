f313cbd90b473155d6a6aa99f3059711f3042111
Nikolaos IOANNOU <nikolaos.ioannou.e@thalesdigital.io>
2020-04-01 10:18:01 +0200

    add role birthdate and location to the when creating the user

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,30 @@
+# https://simpleisbetterthancomplex.com/tutorial/2016/11/23/how-to-add-user-profile-to-django-admin.html
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+class Profile(models.Model):
+    #TODO change role names
+    STUDENT = 1
+    TEACHER = 2
+    SUPERVISOR = 3
+    ROLE_CHOICES = (
+        (STUDENT, 'Student'),
+        (TEACHER, 'Teacher'),
+        (SUPERVISOR, 'Supervisor'),
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    location = models.CharField(max_length=30, blank=True)
+    birthdate = models.DateField(null=True, blank=True)
+    role = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, null=True, blank=True)
+
+    def __str__(self):  # __unicode__ for Python 2
+        return self.user.username
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
+    
