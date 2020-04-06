4a2a39869c86ba6cdfcd8a662d3d61d5298eb356
Nikolaos IOANNOU <nikolaos.ioannou.e@thalesdigital.io>
2020-04-06 14:19:55 +0200

    remove the debug settings

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,4 @@
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from . import views
 
@@ -9,9 +6,3 @@ from . import views
 urlpatterns = [
     path('signup/', views.SignUp.as_view(), name='signup'),
 ]
-
-# Serving the media files in development mode
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-else:
-    urlpatterns += staticfiles_urlpatterns()
